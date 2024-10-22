0fa86f8801a142d844a749de0fcefa7b08547288
Furkan Meclis <furkanmeclis@icloud.com>
2024-10-22 19:34:44 +0300

    some changes

--- app/Http/Controllers/WarrantyController.php
+++ app/Http/Controllers/WarrantyController.php
@@ -9,7 +9,7 @@ class WarrantyController extends Controller
 {
     public function index($serviceNumber = null)
     {
-        return Inertia::render('Warranty/Index',[
+        return Inertia::render('Warranty/Index', [
             'serviceNumber' => $serviceNumber
         ]);
     }
@@ -21,23 +21,27 @@ class WarrantyController extends Controller
     {
 
         $serviceDetails = new Worker\ServicesController();
-        $serviceDetails = $serviceDetails->pdfSourceDataService($serviceNumber,true);
-        if($serviceDetails['status']){
-            $serviceDetails = (object) $serviceDetails['data'];
+        $serviceDetails = $serviceDetails->pdfSourceDataService($serviceNumber, true);
+        if ($serviceDetails['status']) {
+            $serviceDetails = (object)$serviceDetails['data'];
             PDF::setOptions([
                 'isRemoteEnabled' => true,
                 'defaultFont' => 'dejavusans'
             ]);
-            $olexLogoWithText = "data:image/png;base64," .base64_encode(file_get_contents(storage_path('app/public/logos/with_text.png')));
-            $brandLogo = file_get_contents($serviceDetails->brand_logo);
+            $olexLogoWithText = "data:image/png;base64," . base64_encode(file_get_contents(storage_path('app/public/logos/with_text.png')));
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $serviceDetails->brand_logo);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $brandLogo = curl_exec($ch);
+            curl_close($ch);
             $brandLogo = base64_encode($brandLogo);
-            $brandLogo = 'data:image/png;base64,'.$brandLogo;
-            $page2TableLogo = "data:image/png;base64," .base64_encode(file_get_contents(storage_path('app/public/logos/page_2_table.png')));
-            $page2Logo = "data:image/png;base64," .base64_encode(file_get_contents(storage_path('app/public/logos/page_2_logo.png')));
-            $page2Check = "data:image/png;base64," .base64_encode(file_get_contents(storage_path('app/public/logos/page_2_check.png')));
-            $page3 = "data:image/png;base64," .base64_encode(file_get_contents(storage_path('app/public/logos/3-4-1.png')));
-            $page1 = "data:image/png;base64," .base64_encode(file_get_contents(storage_path('app/public/logos/3-4-2.png')));
-            $pdf = PDF::loadView('pdf',[
+            $brandLogo = 'data:image/png;base64,' . $brandLogo;
+            $page2TableLogo = "data:image/png;base64," . base64_encode(file_get_contents(storage_path('app/public/logos/page_2_table.png')));
+            $page2Logo = "data:image/png;base64," . base64_encode(file_get_contents(storage_path('app/public/logos/page_2_logo.png')));
+            $page2Check = "data:image/png;base64," . base64_encode(file_get_contents(storage_path('app/public/logos/page_2_check.png')));
+            $page3 = "data:image/png;base64," . base64_encode(file_get_contents(storage_path('app/public/logos/3-4-1.png')));
+            $page1 = "data:image/png;base64," . base64_encode(file_get_contents(storage_path('app/public/logos/3-4-2.png')));
+            $pdf = PDF::loadView('pdf', [
                 'carSvg' => $this->svgService()->fillCar($serviceDetails->body_data),
                 'brandLogo' => $brandLogo,
                 'brandName' => $serviceDetails->brand,
@@ -53,8 +57,8 @@ class WarrantyController extends Controller
                 'page1' => $page1,
             ]);
             return $pdf->stream('pdf.pdf');
-        }else{
-            return redirect()->route('warranty.index',$serviceNumber);
+        } else {
+            return redirect()->route('warranty.index', $serviceNumber);
         }
     }
 }
